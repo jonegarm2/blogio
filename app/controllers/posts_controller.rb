b82df661d8a998265dca249a318f3acf64f07051
Jonathan Garman <jonjon@Taylors-MacBook-Pro-2.local>
2018-04-10 18:40:40 -0700

    add new post path

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,17 @@ class PostsController < ApplicationController
     end
 
     def new
-        @posts = Post.new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(params.require(:post).permit(:title, :content))
+
+        if @post.save
+            redirect_to root_path
+        else
+            render :new
+        end
     end
     
 end
